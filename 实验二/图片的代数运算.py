fab837eef70483a35827c81b231d66a2b57092b1
DrRem <fffeifei151@gmail.com>
2021-04-27 15:30:43 +0800

    DrRem

--- 实验二/图片的代数运算.py
+++ 实验二/图片的代数运算.py
@@ -1,1 +1,8 @@
 from PIL import Image
+
+if __name__ == "__main__":
+    img1 = Image.open('4.2.05.tiff')
+    assert isinstance(img1, Image.Image)
+    img2 = Image.open('4.2.06.tiff')
+    assert isinstance(img2, Image.Image)
+    print(img1.size, img2.size)
